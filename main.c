f1b8d45ea7032e37e803ce4280c55e1000238fc2
MikkaK <mikka.kummer@edu.tbz.ch>
2021-12-08 15:54:28 +0100

    funktionierende version

--- main.c
+++ main.c
@@ -218,7 +218,7 @@ int checker(char *a, int n) {
 
                 }
                 else{
-                    printf("Ungueltige Eingabe");
+                    printf("Ungueltige Eingabe\n");
                 }
     }
     return n;
