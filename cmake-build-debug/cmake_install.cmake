1aee5232b401173f6f1ba4c6530e5babafb54c77
MikkaK <mikka.kummer@edu.tbz.ch>
2021-12-08 16:03:01 +0100

    try to commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/cedzo/C-Rechner
+# Install script for directory: C:/Users/kumme/Desktop/Noser/C-Rechner/C-Rechner
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/cedzo/C-Rechner/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/kumme/Desktop/Noser/C-Rechner/C-Rechner/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
