b8fd2d175c979710b6ffdbcb9daed6fae1af9878
Cedzol <90751132+Cedzol@users.noreply.github.com>
2021-12-08 12:50:49 +0100

    2D Flächen bis dreieck

---
+++ cmake-build-debug/CMakeFiles/C_Rechner.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Rechner.dir/3D.c.obj"
+  "CMakeFiles/C_Rechner.dir/main.c.obj"
+  "C_Rechner.exe"
+  "C_Rechner.exe.manifest"
+  "C_Rechner.pdb"
+  "libC_Rechner.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Rechner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
