2b3e5c26dc0f11ef902fa7fb624dd611769bc302
Cedzol <90751132+Cedzol@users.noreply.github.com>
2021-12-08 11:07:03 +0100

    cmake minimum

--- 3D.c
+++ 3D.c
@@ -2,6 +2,16 @@
 // Created by kumme on 08.12.2021.
 //
 
+#include <malloc.h>
+#include <string.h>
+#include <ctype.h>
 #include "3D.h"
+#include <stdio.h>
+
+double kreis(){
+    double durchmesserk;
+    scanf("%lf%*[^0-9]",  &durchmesserk);
+    printf("%lf", durchmesserk);
+}
 
 
