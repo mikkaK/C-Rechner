17e6ac26338fbab77ed9609373850077cf6e9e38
Cedzol <90751132+Cedzol@users.noreply.github.com>
2021-12-09 11:47:23 +0100

    2D inputs cant be smaller than 0 now

--- 2D.c
+++ 2D.c
@@ -8,10 +8,17 @@
 
 double circle(){
     double kDiameter;
+    dCheck:
     printf("Gib den Durchmesser deines Kreises an: \n");
     while (scanf("%lf%*[0-9]", &kDiameter) != 1){
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
+    }
+
+    if (kDiameter < 0){
+        printf("Eingabe darf nicht kleiner als 0 sein\n");
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        goto dCheck;
     }
 
     double kRadius = kDiameter / 2;
@@ -22,10 +29,17 @@ double circle(){
 
 double square(){
     double sSquare;
+    squareCheck:
     printf("Gib die Seitenl\x84nge deines Quadrates an: \n");
     while (scanf("%lf%*[0-9]",  &sSquare) != 1){
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
+    }
+
+    if (sSquare < 0){
+        goto squareCheck;
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        printf("Eingabe darf nicht kleiner als 0 sein\n");
     }
 
     double aSquare = sSquare * sSquare;
@@ -35,18 +49,34 @@ double square(){
 
 double rectangle(){
     double sRectangleA;
+    rACheck:
     printf("Gib die l\x84nge deines Rechtecks an: \n");
     while (scanf("%lf%*[0-9]",  &sRectangleA) != 1){
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
     }
 
+    if (sRectangleA < 0){
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        printf("Eingabe darf nicht kleiner als 0 sein\n");
+        goto rACheck;
+    }
+
+
     double sRectangleB;
+    rBCheck:
     printf("Gib die breite deines Rechtecks an: \n");
     while (scanf("%lf%*[0-9]",  &sRectangleB) != 1){
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
     }
+
+    if (sRectangleB < 0){
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        printf("Eingabe darf nicht kleiner als 0 sein\n");
+        goto rBCheck;
+    }
+
     double aRectangle = sRectangleA * sRectangleB;
     printf("Die Fl\x84che deines Rechtecks betr\x84gt: %lf\n", aRectangle);
     return 0;
@@ -54,20 +84,37 @@ double rectangle(){
 
 double triangle() {
     double gTriangle;
+    double hTriangle;
+    double aTriangle;
+    gCheck:
     printf("Gib die Grundseite des Dreiecks ein: \n");
     while (scanf("%lf%*[0-9]", &gTriangle) != 1) {
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
 
     }
-    double hTriangle;
+
+    if (gTriangle < 0){
+        printf("Eingabe darf nicht kleiner als 0 sein\n");
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        goto gCheck;
+    }
+
+    hCheck:
     printf("Gib die H\x94he deines Dreiecks an: \n");
     while (scanf("%lf%*[0-9]", &hTriangle) != 1) {
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
     }
 
-    double aTriangle = gTriangle * hTriangle / 2;
+    if (hTriangle < 0){
+        printf("Eingabe darf nicht kleiner als 0 sein\n");
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        goto hCheck;
+
+    }
+
+    aTriangle = gTriangle * hTriangle / 2;
 
     printf("Die Fl\x84che deines Dreiecks betr\x84gt: %lf\n", aTriangle);
 
@@ -77,20 +124,46 @@ double trapez(){
     double trapezGrundlinie;
     double trapezDeckenlinie;
     double trapezHoehe;
+
+    gTrapez:
     printf("Gib die L\x84nge der Grundlinie deines Trapez an (in cm): \n");
     while (scanf("%lf%*[0-9]", &trapezGrundlinie) != 1) {
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
     }
+
+    if (trapezGrundlinie < 0){
+        printf("Eingabe darf nicht kleiner als 0 sein");
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        goto gTrapez;
+
+    }
+
+    dCheck:
     printf("Gib die L\x84nge der Deckenlinie deines Trapez an an (in cm): \n");
     while (scanf("%lf%*[0-9]", &trapezDeckenlinie) != 1) {
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
     }
+
+    if (trapezDeckenlinie < 0){
+        printf("Eingabe darf nicht kleiner als 0 sein\n");
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        goto dCheck;
+
+    }
+
+    hCheck:
     printf("Gib die H\x94he deines Trapez an an (in cm): \n");
     while (scanf("%lf%*[0-9]", &trapezHoehe) != 1) {
         printf("Falsche Eingabe! Versuche es erneut\n");
         while (!feof(stdin) && fgetc(stdin) != '\n');
+    }
+
+    if (trapezHoehe < 0){
+        printf("Eingabe darf nicht kleiner als 0 sein\n");
+        while (!feof(stdin) && fgetc(stdin) != '\n');
+        goto hCheck;
     }
     double trapez = ((trapezGrundlinie + trapezDeckenlinie) / 2) * trapezHoehe;
     printf("Das Volumen deines Trapez ist: %lf cm^3\n", trapez);
